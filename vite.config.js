310bbaf28badb76ffc0388bb6e7f35c90fa32a81
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-11-09 13:08:10 +0100

    check

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 // vite.config.js
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "./",
-});
+export default defineConfig(({ mode }) => ({
+  base: mode === "github" ? "/Jobliss-Stiches/" : "./",
+  server: {
+    sourcemap: false,
+    mime: {
+      // Set the MIME type for .js files to application/javascript
+      js: "application/javascript",
+    },
+  },
+  plugins: [
+    tailwindcss(), // Add Tailwind CSS as a plugin
+  ],
+}));
