e545ebd8e827d975c6bcc5bd005c61acfe278a8a
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-11-10 09:09:47 +0100

    "Updated dependencies, removed vite-plugin-external, and modified configurations for vite, tailwind, and postcss."

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import("tailwindcss").Config;
 module.exports = {
   content: [
-    "./**/*.{html,tsx}",
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
     "node_modules/preline/dist/*.js",
     "./node_modules/tw-elements/js/**/*.js",
   ],
@@ -14,8 +14,8 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("preline/plugin"),
-    require("tw-elements/plugin.cjs"),
+    require("@tailwindcss/forms"), // Tailwind forms plugin
+    require("preline/plugin"), // Preline plugin for additional components
+    require("tw-elements/plugin.cjs"), // Tw-elements plugin
   ],
 };
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Jobliss-Stiches/" : "./", // Set base URL differently in production
+  base: mode === "production" ? "/Jobliss-Stiches/" : "/", // Set base URL differently in production
   server: {
     sourcemap: mode === "development", // Enable sourcemaps only in development
+  },
+  css: {
+    postcss: "./postcss.config.js", // Ensure Vite uses your postcss.config.js
   },
   build: {
     rollupOptions: {
@@ -12,7 +15,7 @@ export default defineConfig(({ mode }) => ({
         "https://cdn.jsdelivr.net/npm/tw-elements@2.0.0/js/tw-elements.umd.min.js",
       ],
     },
-    minify: mode === "production" ? "esbuild" : false, // Minify only in production
+    // minify: mode === "production" ? "esbuild" : false, // Minify only in production
   },
   plugins: [tailwindcss()],
 }));
